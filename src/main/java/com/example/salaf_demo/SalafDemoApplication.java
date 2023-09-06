dc52917eb2f44a84e4ffe386f7b282dffa257db8
Nisflei <ncgaloni@hotmail.com>
2023-09-06 17:26:17 -0300

    first commit

---
+++ src/main/java/com/example/salaf_demo/SalafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.salaf_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalafDemoApplication.class, args);
+    }
+
+}
